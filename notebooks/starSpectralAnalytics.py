813bfc6933fde9a374e3db5e5265f5820edfa53e
Shaga Sresthaa <shagasresthaa@gmail.com>
2024-04-08 16:12:48 -0400

    checking out normalized and padded data

--- notebooks/starSpectralAnalytics.py
+++ notebooks/starSpectralAnalytics.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('data/processed/starSpectralNoiseReducedData/1237645942906028124_spectra_denoised.csv')  # Update with your CSV file path
+df = pd.read_csv('data/augmentation/modelData/starNormalizedData/M/1237645942904389833_normalized.csv')  # Update with your CSV file path
     
     
 
 # Assuming your CSV has columns named 'wavelength' and 'flux'
-wavelength = df['wavelength']
+wavelength = df['normalized_wavelength']
 flux = df['flux']
-smoothed_flux = df['smoothed_flux']
+smoothed_flux = df['normalized_smoothed_flux']
 
 # Plotting
 plt.figure(figsize=(10, 6))  # Optional: Adjusts the figure size
